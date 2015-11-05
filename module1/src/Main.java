cd843354b597924bbf90017f0d00e63cc0d63ed6
ArabChen <757938160@qq.com>
2015-11-05 15:32:02 +0800

    提交Main函数

---
+++ module1/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Partner on 2015/11/5.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello this is my first git project");
+    }
+}
