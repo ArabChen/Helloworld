950a024eb68151c008673552b0f935bf05d4a8e3
ArabChen <757938160@qq.com>
2015-11-05 21:04:13 +0800

    update main

--- module1/src/Main.java
+++ module1/src/Main.java
@@ -4,5 +4,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello this is my first git project");
+        System.out.println("this is un update operation");
     }
 }
